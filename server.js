138634ae163a6f09d006793c0140b1be7172d0c1
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-13 01:19:08 +1000

    Db model created

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./Model/ShortUrl')
 const app = express()
 
-app.set('view engine', 'ejs')
-
-app.get("/",(req,res)=>{
-    res.render("index")
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
 })
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
 app.listen(process.env.PORT||5500)
