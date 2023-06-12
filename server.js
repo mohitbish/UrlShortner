51f7c63f638e24c90453e349c39f1b3a0dc22635
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-13 01:19:58 +1000

    short url redirect created

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/', async (req, res) => {
   res.render('index', { shortUrls: shortUrls })
 })
 
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+  
+    shortUrl.clicks++
+    shortUrl.save()
+  
+    res.redirect(shortUrl.full)
+  })
+  
 app.post('/shortUrls', async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl })
 
